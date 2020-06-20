7de4735b541742164dc7c93673d5984ac74a6d47
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2020-06-20 14:38:09 +0530

    add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, redirect
 app = Flask(__name__)  # __name__ is the module name
-friends = ["priyanshi", "priyanka", "neetu", "mahesh"]
+friends = ["priyanshi", "nokia", "neetu", "mahesh"]
+num = 5
 
 
 @app.route("/")
 def hello():
-    return render_template("index.html", my_friends=friends)
+    return render_template("index.html", my_friends=friends, number=num)
 
 
-@app.route("/about")
+@app.route("/terms")
 def about():
-    return "<h1>about page<h1>"
+    return "<h1>about terms<h1>"
 
 
-@app.route("/home")
+@app.route("/policy")
 def home():
     return redirect("/")
 
